f2c2bbf811a8642383024b95a5f926ed51459bcf
icedlee337 <lee.e.norris@gmail.com>
2021-10-26 17:27:04 -0400

    chore: form submission worx

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,17 +2,43 @@
 let dark = true;
 let theme;
 $: theme = 'hero ' + (dark ? 'dark' : 'light');
-function submit () {
-    console.warn(dark)
+function submit(a){
+    const {srcElement} = a;
+    const {elements} = srcElement;
+    const {length} = elements;
+    
+    const value = Object.keys(srcElement.elements)
+        .filter(k => !(/\d/.test(k)))
+        .map(k => ({k, v: getValueFromElement(srcElement.elements[k])}))
+        .reduce((acc, {k,v}) => ({...acc, [k]: v}), {});
+    
+console.warn(value);
+
+    a?.stopPropagation?.();
+    a?.preventDefault?.();
+
+    return false;
+}
+
+function getValueFromElement (thing) {
+    console.warn(thing.value);
+    
+    console.warn(thing.getAttribute('checked'));
+    console.warn(thing.getAttribute('name'));
+    console.warn(thing.getAttribute('type'));
+    return thing.value;
 }
 </script>
 
 <div class={theme}>
     <div>
         <h1 class="txt">Welcome to SvelteKit</h1>
-        <form class="col txt justify-content-space-between align-items-center" style="height: 100%;">
-            <div class="row justify-content-space-between align-items-center"><input type="checkbox" bind:checked={dark} id="dark" /><label for="dark">Dark?</label></div>
-            <div><button class="btn-1" on:click|preventDefault={submit}>Submit</button></div>
+        <form class="col txt pad-3 justify-content-space-between align-items-center" style="height: 100%;" on:submit={submit}>
+            <div class="row justify-content-space-between align-items-center"><input name="dark" class="btn-1" type="checkbox" bind:checked={dark} id="dark" on:change={v => console.warn(v.target.getAttribute('checked'))}/><label class="btn-1" for="dark">Dark? {dark}</label></div>            
+            <div class="row"><input name="age" required type="number"/><label>age</label></div>
+            <div class="row"><input name="name" type="text"/><label>name</label></div>
+            <input class="btn-1" type="submit" />
+            
         </form>
     </div>
 </div>
@@ -26,6 +52,11 @@ function submit () {
     .hero {
         background-image: url('http://localhost:4443/assets/logo.svg');
         height: 90vh;        
+    }
+
+    .btn-1 {
+        background-color: hotpink;
+        @extend .blackice;
     }
 
     form {
